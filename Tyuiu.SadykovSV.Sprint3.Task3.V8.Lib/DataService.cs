cee560dfeaba15eb1acc5ae733de5f707578a405
ssssadykov86@gmail.com <ssssadykov86@gmail.com>
2026-09-21 00:02:29 +0500

    Task3

--- Tyuiu.SadykovSV.Sprint3.Task3.V8.Lib/DataService.cs
+++ Tyuiu.SadykovSV.Sprint3.Task3.V8.Lib/DataService.cs
@@ -1,4 +1,5 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
 namespace Tyuiu.SadykovSV.Sprint3.Task3.V8.Lib
 {
     public class DataService : ISprint3Task3V8
--- Tyuiu.SadykovSV.Sprint3.Task3.V8.Test/DataServiceTest.cs
+++ Tyuiu.SadykovSV.Sprint3.Task3.V8.Test/DataServiceTest.cs
@@ -1,4 +1,5 @@
 ﻿using Tyuiu.SadykovSV.Sprint3.Task3.V8.Lib;
+
 namespace Tyuiu.SadykovSV.Sprint3.Task3.V8.Test
 {
     [TestClass]
