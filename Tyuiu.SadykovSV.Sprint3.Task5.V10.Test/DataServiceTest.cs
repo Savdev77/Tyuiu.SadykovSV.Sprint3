8b1246923be01a349beeed176e34a2b6982ee5a3
ssssadykov86@gmail.com <ssssadykov86@gmail.com>
2026-09-22 00:14:07 +0500

    Task5

---
+++ Tyuiu.SadykovSV.Sprint3.Task5.V10.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.SadykovSV.Sprint3.Task5.V10.Lib;
+namespace Tyuiu.SadykovSV.Sprint3.Task5.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSumSeries()
+        {
+            DataService ds = new DataService();
+            int x = 5;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 10;
+            double res = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+            double expected = 20685;
+
+            Assert.AreEqual(expected, res);
+        }
+    }
+}
---
+++ Tyuiu.SadykovSV.Sprint3.Task5.V10/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.SadykovSV.Sprint3.Task5.V10.Lib;
+Console.Title = "Спринт #3 | Выполнил: Садыков С.В. | ПИНб-26-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Вложенные циклы                                                   *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #10                                                             *");
+Console.WriteLine("* Выполнил: Садыков С.В. | ПИНб-26-1                                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Вычислить сумму ряда y = сумма от i=1 до 3 (сумма от k=1 до 10          *");
+Console.WriteLine("* (x^3 * k) + 2) при x = 5.                                               *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int x = 5;
+int startValue1 = 1;
+int stopValue1 = 3;
+int startValue2 = 1;
+int stopValue2 = 10;
+
+Console.WriteLine($"Переменная x = {x}");
+Console.WriteLine($"Старт внешнего цикла i = {startValue1}");
+Console.WriteLine($"Конец внешнего цикла i = {stopValue1}");
+Console.WriteLine($"Старт внутреннего цикла k = {startValue2}");
+Console.WriteLine($"Конец внутреннего цикла k = {stopValue2}");
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+DataService ds = new DataService();
+double res = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+
+Console.WriteLine($"Сумма двойного ряда равна: {res}");
+
+Console.ReadKey();
